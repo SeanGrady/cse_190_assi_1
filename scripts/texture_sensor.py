f24c3fde0d4d753eebe10cb261e48b600af867bd
Sean Grady <vedicmonk@gmail.com>
2016-03-31 18:47:23 -0700

    Added skeleton for texture sensor node, need to add in noise

---
+++ scripts/texture_sensor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rospy
+import random as r
+import math as m
+import numpy as np
+from copy import deepcopy
+from cse_190_assi_1.srv import requestMapData, textureService
+
+class TexSensor():
+    def __init__(self):
+        rospy.init_node("temperature_sensor")
+        self.texture_requester = rospy.ServiceProxy(
+                "requestMapData",
+                requestMapData
+        )
+        self.texture_service = rospy.Service(
+                "textureService",
+                textureService,
+                self.handle_texture_request
+        )
+        rospy.spin()
+
+    def handle_texture_request(self, request):
+        texture = self.take_measurement()
+        noisy_texture = self.add_noise(texture)
+        return noisy_texture
+
+    def take_measurement(self):
+        tex_response = self.texture_requester('tex')
+        tex = tex_response.data
+        return tex
+
+    def add_noise(self, measurement):
+        pass
+
+
+if __name__ == '__main__':
+    ts = TexSensor()
