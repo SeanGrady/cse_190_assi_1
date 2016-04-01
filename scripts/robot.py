cb14c102391ebaec3ca234095dbac219ede3fb26
Sean Grady <vedicmonk@gmail.com>
2016-04-01 14:46:46 -0700

    added texture update function

--- scripts/robot.py
+++ scripts/robot.py
@@ -35,7 +35,7 @@ class RobotController():
     def initialize_constants(self):
         self.prob_texture_correct = 0.99
         self.prob_move_correct = 1.0
-        std_dev_temp_sensor = 10
+        self.std_dev_temp_sensor = 10
 
     def set_motion_commands(self):
         self.move_list = [[0,0],[0,1],[1,0],[1,0],[0,1],[0,1]]
@@ -107,7 +107,20 @@ class RobotController():
         pass
 
     def update_from_tex(self, texture_reading):
-        pass
+        temp_probs = deepcopy(self.probability_matrix)
+        num_cols = len(temp_probs[0])
+        num_rows = len(temp_probs)
+        for i in range(num_rows):
+            for j in range(num_cols):
+                hit = int(self.texture_map[i][j] == texture_reading)
+                temp_probs[i][j] = (self.prob_texture_correct * hit + (1 - prob_texture_correct) * (1 - hit)) * self.probability_matrix[i][j]
+
+        total_prob = sum(sum(p) for p in temp_probs)
+        if total_prob == 0:
+            print "sum of probabilities is zero"
+        else:
+            temp_probs = [[prob/total_prob for prob in row] for row in temp_probs]
+        self.probability_matrix = temp_probs
 
     def request_texture_reading(self):
         response = self.texture_requester()
