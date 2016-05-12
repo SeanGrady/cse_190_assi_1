51cd663a578086ef72bd8afe7027ab3b35a6ecb1
joemanley201 <joemanley201@gmail.com>
2016-05-12 16:29:29 -0700

    Update image_util to read from img and save to saved_video one level above

--- scripts/image_util.py
+++ scripts/image_util.py
@@ -4,13 +4,13 @@ import os
 
 MAP_SHAPE = (244, 244, 3)
 
-up = cv2.imread("img/up.jpg")
-down = cv2.imread("img/down.jpg")
-left = cv2.imread("img/left.jpg")
-right = cv2.imread("img/right.jpg")
-goal = cv2.imread("img/goal.jpg")
-wall = cv2.imread("img/wall.jpg")
-pit = cv2.imread("img/pit.jpg")
+up = cv2.imread("../img/up.jpg")
+down = cv2.imread("../img/down.jpg")
+left = cv2.imread("../img/left.jpg")
+right = cv2.imread("../img/right.jpg")
+goal = cv2.imread("../img/goal.jpg")
+wall = cv2.imread("../img/wall.jpg")
+pit = cv2.imread("../img/pit.jpg")
 
 img_map = {
     "WALL": wall,
@@ -25,6 +25,7 @@ img_map = {
 height, width, layers = MAP_SHAPE
 
 def save_image_for_iteration(policy_list, iteration):
+    #Creating an empty map of white spaces
     empty_map = np.zeros(MAP_SHAPE)
     empty_map.fill(255)
     for row in range(len(policy_list)):
@@ -32,15 +33,16 @@ def save_image_for_iteration(policy_list, iteration):
             new_pos_row = ((row + 1) * 4) + (row * 20)
             new_pos_col = ((col + 1) * 4) + (col * 20)
             empty_map[new_pos_row : new_pos_row + 20, new_pos_col : new_pos_col + 20] = img_map[policy_list[row][col]]
-    cv2.imwrite("saved_video/iteration_" + str(iteration - 1) + ".jpg", empty_map)
+    cv2.imwrite("../saved_video/iteration_" + str(iteration) + ".jpg", empty_map)
 
 
 def generate_video(no_of_iterations):
-    video = cv2.VideoWriter("saved_video/video.avi", cv2.cv.CV_FOURCC('m', 'p', '4', 'v'), 0.75, (width, height))
+    video = cv2.VideoWriter("../saved_video/video.avi", cv2.cv.CV_FOURCC('m', 'p', '4', 'v'), 1, (width, height))
 
     for i in range(no_of_iterations):
-        file_name = "saved_video/iteration_" + str(i) + ".jpg"
+        file_name = "../saved_video/iteration_" + str(i) + ".jpg"
         img = cv2.imread(file_name)
         video.write(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
+        #This removes the image after stitching it to the video. Please comment this if you want the images to be saved
         os.remove(file_name)
     video.release()
